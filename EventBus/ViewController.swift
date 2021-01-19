3d3cf2b103911ab23dee9554e58bc2f2cd7f6a0c
Dmytro Rebenko <dima.rebenko@gmail.com>
2021-01-19 11:38:01 +0200

    Initial Commit

---
+++ EventBus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EventBus
+//
+//  Created by AndAdmin on 19.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
